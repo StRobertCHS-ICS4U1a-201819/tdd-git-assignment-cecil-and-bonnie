d089b6ff007801b04658b34ae942ca7761a48d59
Bonnie Li <bonnieli20010901@gmail.com>
2018-11-10 22:22:56 -0500

    pass second general test case for median function

--- statTools.py
+++ statTools.py
@@ -1,3 +1,12 @@
 def median(num_list):
     num_list.sort()
-    return 3
+    if len(num_list) % 2 == 0:
+        mid_1 = int(len(num_list) / 2)
+        mid_2 = int(len(num_list) / 2 + 1)
+        median = (num_list[mid_1 - 1] + num_list[mid_2 - 1]) /2
+    else:
+        mid = int((len(num_list) + 1) / 2)
+        median = num_list[mid-1]
+    return median
+
+    
--- test_statTools.py
+++ test_statTools.py
@@ -3,3 +3,6 @@ from statTools import *
 
 def test_median_basic1():
     assert(median([1, 2, 3, 4, 5]) == 3)
+
+def test_median_basic2():
+    assert(median([1, 2, 3, 4, 5, 6]) == 3.5)
