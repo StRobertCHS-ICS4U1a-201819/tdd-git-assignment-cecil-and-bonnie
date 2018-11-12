8ee5d3d086506408b9851278b1899b9c3b3ebd5c
pokemonball34 <pokemonball34@gmail.com>
2018-11-11 23:26:20 -0500

    Refactor code, add documentation

--- statTools.py
+++ statTools.py
@@ -1,95 +1,168 @@
+"""
+------------------------------------------------------------------------------------------
+Name:		statTools.py
+Purpose:	To be able to calculate Measures of Central Tendency and Measures of Spread.
+
+
+Author:		Cecil.Cao, Bonnie.Li
+
+Created:	11/11/2018
+------------------------------------------------------------------------------------------
+"""
+
+
 def mean(num_list):
+    """ Calculate the average of a given list of numbers
+      :param num_list: list The list of numbers needed to calculate the mean
+      :return: int/float The average of the list of numbers
+    """
+    # Checks if the inputted list is empty, if it does, raise an exception and exit the function
     if len(num_list) == 0:
+        # If there is an empty list, return an error message and skip the rest of the function
         raise ValueError("Illegal empty list")
         pass
+    # The sum of all the numbers
     total = 0
+    # Try if we can add all the numbers in the list together
     try:
+        # Adds the numbers together from num_list
         for number in num_list:
             total += number
+    # If the list has something other than an int or float, raise a TypeError exception
     except TypeError:
+        # Print an error message and end the code
         raise TypeError("A number was not provided.")
+        pass
+    # Calculates the mean
     total = total / len(num_list)
     return total
 
 
 def mode(num_list):
+    """ Calculate the most recurring number from a list of numbers.
+          :param num_list: list The list of numbers used to calculate the mode
+          :return: int/list Returns the most recurring number(s) from num_list
+    """
+    # Checks if the list is empty, if it is, raise an exception and end the program
     if len(num_list) == 0:
         raise ValueError("Illegal empty list")
         pass
     num_list.sort()
     current_num = num_list[0]
     current_count = 0
+    # The highest occurrence of a number
     highest_count = 0
+    # Used to check all the unique numbers in num_list
     unique_numbers = []
+    # Used to check how many times a certain number pops up in the list
     num_frequency = []
+    # Used last to determine the most frequent number(s) for mode
     most_frequent_num = []
     for count in range(len(num_list)):
+        # Checks each element in the list is an int or a float if not, raise an exception
         if not(type(num_list[count]) is int or type(num_list[count]) is float):
             raise TypeError("An integer or float was not provided.")
             pass
 
+        # Checks if the number being checked is the same as the previous
         if current_num == num_list[count]:
+            # Increment by 1 if True
             current_count += 1
+            # If the high score is less than the current score, change highest score to current score
             if highest_count < current_count:
                 highest_count = current_count
 
+        # Check if current number isn't equal to previous number
         elif current_num != num_list[count]:
+            # If so, change current number to new number and add that number to the unique numbers list
             unique_numbers.append(current_num)
             num_frequency.append(current_count)
             current_num = num_list[count]
             current_count = 1
 
+        # Finally check if we reach the end of num_list and add the final increment onto the 2 other lists.
         if count == len(num_list) - 1:
             unique_numbers.append(current_num)
             num_frequency.append(current_count)
 
+    # Checks which numbers occurred the most out of the set of unique numbers and adds it to the most_frequent_num list
     for counter in range(len(unique_numbers)):
         if num_frequency[counter] == highest_count:
             most_frequent_num.append(unique_numbers[counter])
 
+    # If there's only one number in the list, return that number as an integer
     if len(most_frequent_num) == 1:
         return most_frequent_num[0]
 
+    # Otherwise, if there's more than one number, return the most_frequent_num list
     else:
         return most_frequent_num
 
 
 def stat_range(num_list):
+    """ Calculate the difference between the largest from the smallest number in the list
+          :param num_list: list The list of at least 2 numbers needed to calculate the range
+          :return: int/float Returns the difference between the largest and smallest number in the list
+    """
+    # Checks if the string is empty, if it is, raise an exception and end the function
     if len(num_list) == 0:
         raise ValueError("Illegal empty list")
         pass
+
+    # Checks if there is only one number in the list, if so, raise an exception and end the function
     elif len(num_list) == 1:
         raise ValueError("Only one element in list when there should be two or more.")
         pass
+
+    # Tests if the list contains only floats or integers, otherwise, raise an exception
     try:
+        # After sorting the list, take the lowest number and highest number and subract the difference
         num_list.sort()
         lowest_num = num_list[0]
         highest_num = num_list[len(num_list) - 1]
+        # Return the difference to the user
         return highest_num - lowest_num
+    # Raises the exception if program finds non-integer/non-float
     except TypeError:
         raise TypeError("An integer or float was not provided.")
 
 
 def lowerQuartile(num_list):
+    """ Calculate the median of the smaller half of the list
+          :param num_list: list The list of numbers given to calculate lower quartile
+          :return: int/float The median of the smaller half of the list
+    """
+    # Checks if the list has less than 4 elements, if it does, raise an exception
     if len(num_list) < 4:
+        """
+        Since we cannot calculate a lower quartile with less than 4 values,
+        should he user input a list with 3 or less elements, we need to raise an exception
+        and end function
+        """
         raise ValueError("Less than 4 numbers when there should be more than 4.")
         pass
 
+    # Sorts the list and checks the position of the median
     num_list.sort()
     lower_half = (len(num_list) // 2)
     lower_half_list = []
+    # Runs a loop to check each value and stores it if it's below the halfway mark of the list
     for element in range(len(num_list)):
+        # If the element is not a integer or float, raise an exception and end the program
         if type(num_list[element]) is not float and type(num_list[element]) is not int:
             raise TypeError("An integer or float was not provided.")
             pass
-        elif len(num_list) % 2 == 1:
-            if element < lower_half:
-                lower_half_list.append(num_list[element])
-        else:
-            if element < lower_half:
-                lower_half_list.append(num_list[element])
+
+        # Takes in all numbers that are before the halfway point of the list and put them into lower_half_list
+        if element < lower_half:
+            lower_half_list.append(num_list[element])
+
+    # Sets the median position of the new list
     lower_half_median_position = len(lower_half_list) // 2
+    # Checks if the new list is odd or even
     if len(lower_half_list) % 2 == 1:
+        # If odd, return the number in the middle position of the list
         return lower_half_list[lower_half_median_position]
     else:
+        # If even, add the numbers adjacent to the median and calculate the average. Return that value
         return (lower_half_list[lower_half_median_position] + lower_half_list[lower_half_median_position - 1]) / 2
--- test_statTools.py
+++ test_statTools.py
@@ -1,128 +1,168 @@
+"""
+------------------------------------------------------------------------------------------
+Name:		test_statTools.py
+Purpose:	To be able to test the functions being developed in statTools.py
+
+
+Author:		Cecil.Cao, Bonnie.Li
+
+Created:	11/11/2018
+------------------------------------------------------------------------------------------
+"""
+
 import pytest
 from statTools import *
 
 
+# Corner Case: Tests to see if the mean function can properly output through taking in 1 number
 def test_mean_basic1():
     assert(mean([2]) == 2)
 
 
+# General Case: Tests to see if the mean function properly calculates the mean
 def test_mean_basic2():
     assert(mean([1, 2, 3, 4, 5]) == 3)
 
 
+# Corner Case: Tests to see if the mean function properly handles strings
 def test_mean_basic3():
     assert(mean([1, 2.4, 1.6, 9.4]) == 3.6)
 
 
+# Unusual Case: Tests to see if an exception is raised when given an empty string
 def test_mean_emptyList():
     with pytest.raises(ValueError) as errmsg:
         mean([])
     assert("Illegal empty list" == str(errmsg.value))
 
 
+# Unusual Case: Tests to see if an exception is raised when given a string within the list
 def test_mean_wrongType():
     with pytest.raises(TypeError) as errmsg:
         mean([1, 2, "Banana Bread", 4, 5])
     assert("A number was not provided." == str(errmsg.value))
 
 
+# Corner Case: Tests to see if the program can properly handle a list with only zeroes
 def test_mean_onlyZeroes():
     assert(mean([0, 0, 0, 0]) == 0)
 
 
+# General Case: Tests to see if it properly outputs the most frequent occurrence of a number in the list
 def test_mode_basic1():
     assert(mode([1, 2, 3, 3]) == 3)
 
 
+# General Case: Tests to see if it properly sorts the list and outputs the most frequent number; 5
 def test_mode_basic2():
     assert(mode([5, 2, 3, 4, 5]) == 5)
 
 
+# Corner Case: Checks if the function can properly handle a list of only zeroes
 def test_mode_onlyZeroes():
     assert(mode([0, 0, 0]) == 0)
 
 
+# Unusual Case: Checks if the function properly raises an exception when given an empty string
 def test_mode_emptyList():
     with pytest.raises(ValueError) as errmsg:
         mode([])
     assert("Illegal empty list" == str(errmsg.value))
 
 
+# Unusual Case: Checks if the function properly raises an exception when given an empty string
 def test_mode_wrongType():
     with pytest.raises(TypeError) as errmsg:
         mode(["Is", "this", "the", "Krusty", "Krab?"])
     assert("An integer or float was not provided." == str(errmsg.value))
 
 
+# General Case: Checks if the function works and can output multiple answers if there are any
 def test_mode_multipleAnswers():
     assert(mode([1, 1, 2, 3, 3]) == [1, 3])
 
 
+# Corner Case: Checks if the function properly outputs the list if the given list has numbers that all occur only once
 def test_mode_equalOccurances():
     assert(mode([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5])
 
 
+# General Case: Checks to see if the program properly calculates the range of the given list
 def test_stat_range_basic1():
     assert(stat_range([1, 2, 7]) == 6)
 
 
+# General Case: Checks if the functions sorts the list and properly calculates the range of the given list
 def test_stat_range_basic2():
     assert(stat_range([11, 2, 1, 10]) == 10)
 
 
+# Unusual Case: Checks if the function raises an exception when given an empty list
 def test_stat_range_emptyList():
     with pytest.raises(ValueError) as errmsg:
         stat_range([])
     assert("Illegal empty list" == str(errmsg.value))
 
 
+# Unusual Case: Checks if the function raises an exception when there is are strings in the given list
 def test_stat_range_wrongType():
     with pytest.raises(TypeError) as errmsg:
         stat_range(["Space is infinite", "but the stars are finite"])
     assert("An integer or float was not provided." == str(errmsg.value))
 
 
+# General Case: Checks if the function works as intended and can use negative numbers
 def test_stat_range_negativeNumbers():
     assert(stat_range([-10, 5, 10, 2]) == 20)
 
 
+# Corner Case: Checks if the function is able to handle a list with only zeroes
 def test_stat_range_onlyZeroes():
     assert(stat_range([0, 0, 0, 0, 0]) == 0)
 
 
+# Unusual Case: Checks if the function raises an exception when given 1 element in the list
+# A range cannot be calculated with only 1 integer or float
 def test_stat_range_oneElement():
     with pytest.raises(ValueError) as errmsg:
         stat_range([1])
     assert("Only one element in list when there should be two or more." == str(errmsg.value))
 
 
+# General Case: Checks if the function properly outputs a lower quartile from an odd numbered list
 def test_lowerQuartile_basic1():
     assert(lowerQuartile([1, 2, 3, 4, 5, 6, 7]) == 2)
 
 
+# General Case: Checks if the function properly works using an even numbered list
 def test_lowerQuartile_basic2():
     assert(lowerQuartile([2, 3, 4, 4, 6, 7, 7, 7, 8, 8, 9, 10]) == 4)
 
 
+# General Case: Checks if the function works given an even numbered list and calculates the average between them
 def test_lowerQuartile_basic3():
     assert(lowerQuartile([1, 2, 3, 4]) == 1.5)
 
 
+# Unusual Case: Checks if the function raises an exception when a string is in the list
 def test_lowerQuartile_wrongType():
     with pytest.raises(TypeError) as errmsg:
         lowerQuartile(["Just", "let", "it", "go"])
     assert("An integer or float was not provided." == str(errmsg.value))
 
 
+# Unusual Case: Checks if the function raises an exception when less than 4 numbers are in the list
 def test_lowerQuartile_lessthan4elements():
     with pytest.raises(ValueError) as errmsg:
         lowerQuartile([1, 2, 3])
     assert("Less than 4 numbers when there should be more than 4." == str(errmsg.value))
 
 
+# Corner Case: Checks if the program can handle a list of only zeroes
 def test_lowerQuartile_onlyZeroes():
     assert(lowerQuartile([0, 0, 0, 0, 0]) == 0)
 
 
+# General Case: Checks if the program can properly calculate the answer given negative numbers
 def test_lowerQuartile_negativeNumbers():
     assert(lowerQuartile([-100, -10, -5, 10, 90]) == -55)
